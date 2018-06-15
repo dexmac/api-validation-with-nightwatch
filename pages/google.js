ef346bbc0af518bacfddf534d1b2e2280dbcc7a2
dexmac <weileru@gmail.com>
2018-06-15 07:53:17 +0300

    Adding autocompleteSuggestions selector

--- pages/google.js
+++ pages/google.js
@@ -1,5 +1,5 @@
 'use strict';
-
+  
 module.exports = {
   url: 'http://www.google.com/ncr', // Going to main google page ('ncr' == no country-redirect)
   elements: {
@@ -7,7 +7,11 @@ module.exports = {
       selector: 'input[type=text]'
     },
     submit: {
-      selector: '//button[@name="btnG"]',
+      selector: '//*[@id="tsf"]/div[2]/div[3]/center/input[1]',
+      locateStrategy: 'xpath'
+    },
+    autocompleteSuggestions: {
+      selector: '//*[@id="sbtc"]/div[2]',
       locateStrategy: 'xpath'
     }
   }
